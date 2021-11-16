a49ac3c4a4318f4bbd1c1fd937d44f5ccb6e0734
VRGID <92531436+VRGID@users.noreply.github.com>
2021-11-16 16:18:14 +0200

    Add files via upload

---
+++ dz_js 10/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function blocks(squareBlue) {
+for (let i = 0; i < 100; i++) {
+     $('<div></div>').css({
+            'height': '70px',
+            'width': '70px',
+            'background': '#0091ff',
+            'margin': '10px'
+        }).appendTo('body');
+}
+
+
+function  blocks2 (squarePurple) {
+     $('<div></div>').css({
+        'height' : '100px',
+        'width' : '100px',
+        'background' : '800080',
+        'margin': '10px'
+    });
+    return;
+}
+})
+
+
+//setTimeout(blocks2, 4000, 100);
